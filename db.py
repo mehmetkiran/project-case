8a13a0c6ac7639edf92735ab88a66d2e2da6c9ee
mehmet kiran <krn.mhmt@gmail.com>
2025-05-28 22:55:43 +0300

    upload db conf

--- db.py
+++ db.py
@@ -1,19 +1,25 @@
 import os
+
+import gridfs
 from dotenv import load_dotenv
+from pymongo import MongoClient
 from sqlmodel import SQLModel, create_engine, Session
 
 from app.models.chat import Base
 
 load_dotenv()
 
-db_name = os.environ.get("POSTGRES_DB", None)
-db_user = os.environ.get("POSTGRES_USER", None)
-db_password = os.environ.get("POSTGRES_PASSWORD", None)
-db_host = os.environ.get("POSTGRES_HOST", None)
+DB_NAME = os.environ.get("POSTGRES_DB", None)
+DB_USER = os.environ.get("POSTGRES_USER", None)
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD", None)
+DB_HOST = os.environ.get("POSTGRES_HOST", None)
 
-DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+MONGO_DB_URL = os.environ.get("MONGO_URI", None)
 
 engine = create_engine(DATABASE_URL, echo=True)
+client = MongoClient(MONGO_DB_URL)
+
 
 def get_session():
     with Session(engine) as session:
